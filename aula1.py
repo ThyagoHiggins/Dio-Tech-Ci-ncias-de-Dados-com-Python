02b2dd483656137a7b6f0a51a5c3e3395a58aad4
Thyago Higgins <thyago.higgins16@gmail.com>
2023-06-28 19:03:13 -0300

    Entregas

---
+++ aula1.py
@@ -0,0 +1,6 @@
+"""Nome do Capítulo: Defininfo o ambiente e prime
+primeiros passo com python
+Exercicio: "Hello World"
+ obs: Dizem que quem não escreve essa primeira linha quando começa nova linguagem pode ter um azar, rs! Vi python na faculdade e gostei, agora revisitando novamente: olá mundo!"""
+
+print('Hello World')
